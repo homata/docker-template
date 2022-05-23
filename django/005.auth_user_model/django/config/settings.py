1c01b50e4f5823a351d056d557c70bb87808e42c
Hiroshi Omata <op.homata@gmail.com>
2022-05-23 17:06:06 +0900

    Add google analytics

--- django/005.auth_user_model/django/config/settings.py
+++ django/005.auth_user_model/django/config/settings.py
@@ -102,6 +102,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'config.context_processors.google_analytics_key',
             ],
         },
     },
